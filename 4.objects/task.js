a2fbf23d77d3be1e4e59186f66cfe2dc644aaf19
«AlenaKryukova» <asche91@mail.ru>
2021-10-14 20:17:25 +0500

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -6,27 +6,29 @@ function Student(name, gender, age) {
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
 }
-Student.prototype.addMark = function(mark) {
-    if(this.marks === undefined){
-        this.marks = [];
-        this.marks.push(mark);
-    } else {
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    }
+    else {
         this.marks.push(mark);
     }
 }
-Student.prototype.addMarks = function(...mark) {
+Student.prototype.addMarks = function (...rest) {
     if (this.marks === undefined) {
-        this.marks = [];
+        this.marks = [...rest];
     }
-    Student.prototype.getAverage = function() {
-        let sum = 0;
-        for (let i = 0; i < this.marks.length; i++) {
-            sum+=this.marks[i];
-            return sum/this.marks[i];
-        }
+    else {
+        this.marks.push(...rest);
     }
-    Student.prototype.exclude = function(reason) {
-        delete this.subject;
-        delete this.marks;
-        this.excluded = reason;
-    }
+}
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    this.marks.forEach((mark) => {sum += parseInt(mark)});
+    return sum / this.marks.length;
+}
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
